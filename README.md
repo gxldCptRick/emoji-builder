a95cdf518a532e29323852d71ae78d3293170084
Andres Hermilo Carrera <andreshcar@live.com>
2025-01-13 14:20:39 -0700

    Adding file group commands to be able to process text from a file in different ways

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 Simple python script to take in a message and symbols to use to create either a text art message of your string as characters or use of emojis by hard coding them into the script to build these cute little text diagrams.
 
+## Old CLI Way
+
 ```bash
-    > python -m builder -m "Some Message"
+    > python -m builder cli -m "Some Message"
 ---
 # output.txt
 😭😭😭😭😭
@@ -84,5 +86,77 @@ Simple python script to take in a message and symbols to use to create either a 
 😭💖💖💖😭
 😭😭😭😭😭
 ```
+
+## New Way with files
+
+Allows you to emojify your messages using files if you don't want the message to be typed in the cli.
+
+### input.yaml
+
+Yamlm files with the only requirements being a `message` key but allows you to override the following:
+- `background`
+- `foreground`
+- `width`
+- `output`
+
+```yaml
+message: Some Message
+background: 😭
+foreground: 💖
+width: 100
+output: output.txt
+```
+
+### input.yml
+
+Supports the `yml` extensions as well as `yaml`
+
+```yaml
+message: Some Message
+background: 😭
+foreground: 💖
+width: 100
+output: output.txt
+```
+
+### input.json 
+
+Json will share the same structure of the yaml where you need to define the `message` and allows you to override the following:
+- `background`
+- `foreground`
+- `width`
+- `output`
+
+Only required to define the `message` key otherwise it will use the same defaults as the cli.
+
+```json
+{
+    "message": "Some Message",
+    "background": "😭",
+    "foreground": "💖",
+    "width": 100,
+    "output": "output.txt"
+}
+```
+
+### input.txt
+
+We will just read in the text of the file as the message you wish to emojify
+
+```txt
+Some Message
+```
+
+### Command 
+
+The command will be under the `file` subcommand group.
+
+```bash
+    > python -m builder file -i input.yaml
+    > python -m builder file -i input.yml
+    > python -m builder file -i input.json
+    > python -m builder file -i input.txt
+```
+
 
 Very useful if you want to make cute little messages by default the foreground is the 💖 and the background is the 😭 however you can use any arbitrary string as foreground and any arbitrary string as background.
--- builder.py
+++ builder.py
@@ -1,26 +1,83 @@
 from abc import ABC, abstractmethod
+import json
 import click
+import pydantic
 import pyperclip
+import yaml
 
 
-@click.command()
+@click.group()
+def main():
+    # used to denote the main entrypoint for the cli
+    pass
+
+
+def tokenize_and_save_to_file(
+    background: str,
+    foreground: str,
+    message: str,
+    filename: str,
+    width: int,
+):
+
+    tokens = tokenize(message, foreground=foreground, background=background)
+    output_message = "\n".join(token.render(width) for token in tokens)
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(output_message)
+    pyperclip.copy(output_message)
+
+
+@main.command("cli")
 @click.option("--black", "-b", default="💖")
 @click.option("--white", "-w", default="😭")
 @click.option("--message", "-m", required=True)
 @click.option("--width", "-wd", default=12, type=int)
 @click.option("--output", "-o", default="output.txt")
-def main(
-    black: str,
-    white: str,
-    message: str,
-    width: int,
-    output: str,
-):
-    tokens = tokenize(message, foreground=black, background=white)
-    output_message = "\n".join(token.render(width) for token in tokens)
-    with open(output, "w", encoding="utf-8") as f:
-        f.write(output_message)
-    pyperclip.copy(output_message)
+def legacy(black: str, white: str, message: str, width: int, output: str):
+    return tokenize_and_save_to_file(
+        background=white,
+        foreground=black,
+        message=message,
+        filename=output,
+        width=width,
+    )
+
+
+class EmojiRequest(pydantic.BaseModel):
+    message: str
+    foreground: str = "💖"
+    background: str = "😭"
+    width: int = 12
+    output: str = "output.txt"
+
+
+def load_file(filename: str):
+    if filename.endswith(".txt"):
+        with open(filename, "r", encoding="utf-8") as f:
+            message = f.read()
+            return EmojiRequest(message=message)
+    elif filename.endswith(".json"):
+        with open(filename, "r", encoding="utf-8") as f:
+            return EmojiRequest.model_validate(json.load(f))
+    elif filename.endswith(".yaml") or filename.endswith(".yml"):
+        with open(filename, "r", encoding="utf-8") as f:
+            return EmojiRequest.model_validate(yaml.safe_load(f))
+    else:
+        raise ValueError(f"Unsupported file type: {filename}")
+
+
+@main.command("file")
+@click.option("--input-file", "-i", required=True)
+@click.option("--output-file", "-o")
+def transform_from_file(input_file: str, output_file: str | None = None):
+    values = load_file(input_file)
+    return tokenize_and_save_to_file(
+        background=values.background,
+        foreground=values.foreground,
+        message=values.message,
+        filename=output_file or values.output,
+        width=values.width,
+    )
 
 
 class Token(ABC):
